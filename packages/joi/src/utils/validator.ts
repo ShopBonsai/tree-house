8af7809044253cf32e664af5e832118850ee0120
Brent Van Geertruy <brentvangeertruy@outlook.com>
2021-10-02 14:18:27 -0400

    Fix lint issue in Joi

--- packages/joi/src/utils/validator.ts
+++ packages/joi/src/utils/validator.ts
@@ -5,12 +5,12 @@ import * as Joi from '@hapi/joi';
  * @param data - Data to validate against.
  * @param schema - Joi schema.
  */
- export const validateJoiSchema = <T>(data: T, schema: Joi.Schema): void =>
- Joi.validate(data, schema, (err, value) => {
-   if (err) {
-     throw err;
-   }
-   if (!value) {
-     throw new Error('No value to check schema');
-   }
- });
+export const validateJoiSchema = <T>(data: T, schema: Joi.Schema): void =>
+  Joi.validate(data, schema, (err, value) => {
+    if (err) {
+      throw err;
+    }
+    if (!value) {
+      throw new Error('No value to check schema');
+    }
+  });
